a67bfa1ff835e69d7a325a5e34b7d20db1a34b90
wange011 <wange011@gmail.com>
2019-05-30 12:16:44 -0400

    updated model and training process

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tensorflow.contrib.rnn import BasicLSTMCell
-from tensorflow.contrib.rnn import LSTMStateTuple
-
 import os 
 import random
 
@@ -52,27 +49,48 @@ input_dim = 78 * 2
 learning_rate = 0.001
 training_steps = 10000
 batch_size = 15
-num_hidden = 100
+num_hidden = [100, 100, 100, 100]
+num_layers = 2
 
 # Here None is the batch_size
 # Define the Graph Input
 X = tf.placeholder("float", [None, timesteps, input_dim])
-y = tf.placeholder("float", [None, input_dim])
+
+# !!!!!!!Define the weight matrices, use LSTMStateTuple to pass in the weights as LSTM States
+# tf.nn.dynamic_rnn outputs output and cell_state
+# Use MultiRNNCell to instantiate multilayer LSTM (pass in cell_list)
+
 
 # 2 consectutive 2 layer LSTMs
 # Decide how to separate the 2 parameters: note played and articulation
-h = model.TimewiseLSTM(X, num_hidden)
-logits = model.NotewiseLSTM(h, num_hidden)
+h = model.TimewiseLSTM(X, num_hidden[:num_layers - 1])
+outputs = model.NotewiseLSTM(h, num_hidden[num_layers:])
 
 # Define loss and optimizer
-loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y))
+loss = model.LossFunction(outputs, X, batch_size, timesteps, input_dim)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate = learning_rate)
 train_op = optimizer.minimize(loss)
 
 # Training
+display_step = 100
 
 init = tf.global_variables_intitializer()
 
 with tf.session() as sess:
     
     sess.run(init)
+
+    for step in range(1, training_steps + 1):
+        
+        # Get batch of shape
+        batch = utility.getPieceBatch(training, batch_size, timesteps)[1]
+        
+        # !!!!Reshape batch
+        
+        
+        loss = sess.run(train_op, feed_dict={X: batch})
+        
+        if step % display_step == 0 or step == 1:
+            # Calculate batch loss and accuracy
+            print("Step " + str(step) + ", Loss= " + \
+                  "{:.4f}".format(loss))
--- model.py
+++ model.py
@@ -1,14 +1,42 @@
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
+# Try dropout
 def TimewiseLSTM(X, num_hidden):
     
-    lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+    cell_list = []
     
-    outputs, states = rnn.static_rnn(lstm_cell, X, dtype=tf.float32)
+    for i in range(num_hidden):
+        
+        lstm_cell = rnn.BasicLSTMCell(num_hidden[i], forget_bias=1.0)
+        cell_list.append(lstm_cell)
+    
+    multi_lstm_cell = rnn.MultiRNNCell(cell_list)    
+    
+    outputs, states = rnn.dynamic_rnn(multi_lstm_cell, X, dtype=tf.float32)
     
 def NotewiseLSTM(h, num_hidden):
     
-    lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
+    cell_list = []
     
-    outputs, states = rnn.static_rnn(lstm_cell, h, dtype=tf.float32)
+    for i in range(num_hidden):
+        
+        lstm_cell = rnn.BasicLSTMCell(num_hidden[i], forget_bias=1.0)
+        cell_list.append(lstm_cell)
+    
+    multi_lstm_cell = rnn.MultiRNNCell(cell_list)    
+    
+    outputs, states = rnn.dynamic_rnn(multi_lstm_cell, h, dtype=tf.float32)
+    
+def LossFunction(outputs, X, batch_size, timesteps, input_dim):
+    
+    # Remove the first slice of X and last slice of outputs
+    outputs_sliced = tf.slice(outputs, [0, 0, 0], [batch_size, timesteps - 1, input_dim])
+    X_sliced = tf.slice(X, [0, 1, 0], [batch_size, timesteps - 1, input_dim])    
+    
+    cross_entropy = tf.nn.sigmoid_cross_entropy_with_logits(logits=outputs_sliced, labels=X_sliced)        
+    
+    loss = tf.reduce_mean(cross_entropy)
+    log_likelihood = -loss * input_dim    
+    
+    return loss, log_likelihood
